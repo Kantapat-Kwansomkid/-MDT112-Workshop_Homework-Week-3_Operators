9f29a4bc976c1675433ee48c64b9df9f7b32938f
Kantapat-Kwansomkid <72593812+Kantapat-Kwansomkid@users.noreply.github.com>
2021-02-21 14:58:59 +0700

    Add files via upload
    การบ้าน MDT112_Workshop สัปดาห์ที่ 3 เรื่อง Operators
    จัดทำโดย นายกันตพัฒน์ ขวัญสมคิด 63120501002

---
+++ HomeworkWeek3_Operators_63120501002.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace HomeworkWeek3_Operators_63120501002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int password;
+            Console.Write("Input Password : ");
+            password = int.Parse(Console.ReadLine());
+
+            string agency;
+            Console.Write("Input Agency : ");
+            agency = (Console.ReadLine());
+
+            if (password {1000 = 7, 9}, {10 = 2, 4, 6, 7, 8, 9}, {1 = 3, 6, 9})
+            agency = "CIA";
+            {
+                Console.WriteLine("Your Password is True");
+
+             else if (password {1000 != 7, 9}, {10 != 2, 4, 6, 7, 8, 9}, {1 != 3, 6, 9})
+             agency = "FBI","NSA"; 
+             
+                Console.WriteLine("Your Password is False");
+            }
+            
+            if (password {100000 = 4, 5, 6, 7}, {10000 = 1, 3, 5, 7, 9}, {100 = 2, 4, 8})
+            agency = "FBI";
+            {
+                Console.WriteLine("Your Password is True");
+
+             else if (password {100000 != 4, 5, 6, 7}, {10000 != 1, 3, 5, 7, 9}, {100 != 2, 4, 8})
+             agency = "CIA","NSA";
+
+                Console.WriteLine("Your Password is False");
+            }
+
+            if (password {100000, 10000, 1000, 10 = 7}, {100 = 3, 9}, {1 = 1, 2, 3, 5, 6})
+            agency = "NSA";
+            {
+                Console.WriteLine("Your Password is True");
+
+             else if (password {100000, 10000, 1000, 10 != 7}, {100 != 3, 9}, {1 != 1, 2, 3, 5, 6})
+             agency = "CIA","FBI";
+
+                Console.WriteLine("Your Password is False");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
